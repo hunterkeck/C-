2974430ff2e761c7cf37ca28ba163c18cf84222a
Hunter <114008447+hunterkeck@users.noreply.github.com>
2023-04-24 13:24:50 -0500

    Add files via upload

---
+++ Binary Search Algorithm/Binary Search Algorithm/Binary_Search_AlgorithmMain.h
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int binarySearch(vector<int> arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (arr[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    vector<int> arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int target = 5;
+    int index = binarySearch(arr, target);
+    if (index == -1) {
+        cout << "Element not found in the array." << endl;
+    }
+    else {
+        cout << "Element found at index " << index << "." << endl;
+    }
+    return 0;
+}
